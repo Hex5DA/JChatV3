28a737b203c528764e66e2cb8b2dd6237c9d6ea0
^-^ Trevor <wasandland@outlook.com>
2021-09-15 20:18:27 +0100

    Pushing local copy to github.

---
+++ Main.java
@@ -0,0 +1,22 @@
+package java.main.com.github.onlytr3v0r.jchat_v3;
+
+// imports
+import javax.swing.JFrame;
+import javax.swing.JTabbedPane;
+
+public class Main extends JFrame {
+    public static void main(String[] args) {
+        new Main();
+    }
+
+    public Main() {
+        super("Java Chatroom V3");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JTabbedPane tabs = new JTabbedPane();
+        Tab test2 = new Tab();
+        tabs.add("hiiii", test2);
+
+        setVisible(true);
+    }
+}
---
+++ Tab.java
@@ -0,0 +1,12 @@
+package java.main.com.github.onlytr3v0r.jchat_v3;
+
+// imports
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Tab extends JPanel {
+    public Tab() {
+        JLabel test = new JLabel("test lol");
+        this.add(test);
+    }
+}
