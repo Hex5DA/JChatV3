8966d27f7ca1cfe62d861a36c19e02117a8240d7
^-^ Trevor <wasandland@outlook.com>
2021-10-31 11:14:26 +0000

    fixed send bar rescaling added scroll pane for the history box

--- src/main/java/com/github/jchat_v3/client/Tab.java
+++ src/main/java/com/github/jchat_v3/client/Tab.java
@@ -2,17 +2,22 @@ package main.java.com.github.jchat_v3.client;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.ComponentOrientation;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.ScrollPaneConstants;
 
 public class Tab extends JPanel {
     private RecieveThread thread;
@@ -22,7 +27,7 @@ public class Tab extends JPanel {
     private static final Logger LOGGER = Logger.getLogger(Tab.class.getName());
 
     private JButton sendButton;
-    private JTextArea sendBar;
+    private JTextField sendBar;
     private JTextArea history;
 
     public Tab(Connection connection) {
@@ -31,11 +36,13 @@ public class Tab extends JPanel {
         setLayout(new BorderLayout());
         JPanel bottom = new JPanel();
         bottom.setLayout(new BorderLayout());
-        sendBar = new JTextArea();
+        sendBar = new JTextField();
         sendButton = new JButton("Send");
         sendButton.addActionListener(new ClickHandler());
         history = new JTextArea();
         history.setEditable(false);
+        JScrollPane historyScrollPane = new JScrollPane(history, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        historyScrollPane.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
         
         sendBar.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1, true));
         bottom.setBorder(BorderFactory.createEmptyBorder(BORDER_SIZE, BORDER_SIZE, BORDER_SIZE, BORDER_SIZE));
@@ -59,7 +66,7 @@ public class Tab extends JPanel {
             // #endregion
         });
 
-        add(history, BorderLayout.CENTER);
+        add(historyScrollPane, BorderLayout.CENTER);
         add(bottom, BorderLayout.SOUTH);
 
         thread = new RecieveThread();
